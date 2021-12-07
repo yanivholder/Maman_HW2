7fe292667137f61a6ee22bbf6ce90668da3d10f7
Yaniv Holder <yaniv.holder@outlook.com>
2021-12-07 20:25:14 +0200

    WIP - not stable

--- hw2_winter22/Solution.py
+++ hw2_winter22/Solution.py
@@ -1,4 +1,6 @@
 from typing import List
+
+import Utility.DBConnector
 import Utility.DBConnector as Connector
 from Utility.ReturnValue import ReturnValue
 from Utility.Exceptions import DatabaseException
@@ -8,56 +10,216 @@ from Business.Stadium import Stadium
 from psycopg2 import sql
 
 
+def checkNone(attribute):
+    """A function to convert python's None to NULL"""
+    return 'NULL' if not attribute else attribute
+
+
+# TODO: change to_print to False
+def sql_query(query, to_print=True):
+    """A wrapper function to all sql queries to avoid duplications"""
+    conn = None
+    # default values to return if no exception happened
+    ret_val = ReturnValue.OK
+    row_effected, entries = 0, Connector.ResultSet()
+
+    try:
+        conn = Connector.DBConnector()
+        row_effected, entries = conn.execute(query, printSchema=to_print)
+        # TODO: should we always commit?
+        conn.commit()
+    except (DatabaseException.ConnectionInvalid,
+            DatabaseException.database_ini_ERROR,
+            DatabaseException.UNKNOWN_ERROR):
+        return ReturnValue.ERROR
+    except (DatabaseException.CHECK_VIOLATION,
+            DatabaseException.NOT_NULL_VIOLATION):
+        return ReturnValue.BAD_PARAMS
+    except DatabaseException.FOREIGN_KEY_VIOLATION:
+        return ReturnValue.NOT_EXISTS
+    except DatabaseException.UNIQUE_VIOLATION:
+        return ReturnValue.ALREADY_EXISTS
+    except:
+        return ReturnValue.ERROR
+    finally:
+        if conn:
+            # TODO: check how to use rollback
+            conn.close()
+        return ReturnValue.OK, row_effected, entries
+
+
 def createTables():
-    pass
+    # TODO: maybe add VIEWS?
+    # TODO: check how to create id's from separate tables unique
+    # TODO: check if this is legal: Away INTEGER CHECK(Away <> Home)
+    sql_query('''
+        CREATE TABLE Teams(
+            TeamID INTEGER NOT NULL UNIQUE CHECK(TeamID > 0),
+            PRIMARY KEY (TeamID));
+        
+        CREATE TABLE Players(
+            PlayerID INTEGER NOT NULL UNIQUE CHECK(PlayerID > 0),
+            TeamID INTEGER,
+            Age INTEGER NOT NULL CHECK(Age > 0),
+            Height INTEGER NOT NULL CHECK(Height > 0),
+            Preferred_foot TEXT NOT NULL CHECK(Preferred_foot='Right' OR Preferred_foot='Left'),
+            FOREIGN KEY (TeamID) REFERENCES Teams(TeamID) ON DELETE CASCADE,
+            PRIMARY KEY (PlayerID));
+        
+        CREATE TABLE Stadiums(
+            StadiumID INTEGER NOT NULL UNIQUE CHECK(StadiumID > 0),
+            Capacity INTEGER NOT NULL CHECK(Capacity > 0),
+            PRIMARY KEY (StadiumID));
+        
+        CREATE TABLE BelongTo(
+            StadiumID INTEGER UNIQUE,
+            TeamID INTEGER UNIQUE,
+            FOREIGN KEY (StadiumID) REFERENCES Stadiums(StadiumID) ON DELETE CASCADE,
+            FOREIGN KEY (TeamID) REFERENCES Teams(TeamID) ON DELETE CASCADE,
+            PRIMARY KEY (StadiumID, TeamID));
+            
+        CREATE TABLE Match(
+            MatchID INTEGER NOT NULL UNIQUE CHECK(MatchID > 0),
+            Competition TEXT NOT NULL CHECK(Competition='International' OR Competition='Domestic'),
+            Home INTEGER,
+            Away INTEGER CHECK(Away <> Home),
+            FOREIGN KEY (Home) REFERENCES Teams(TeamID) ON DELETE CASCADE,
+            FOREIGN KEY (Away) REFERENCES Teams(TeamID) ON DELETE CASCADE,
+            PRIMARY KEY (MatchID));
+    ''')
 
 
 def clearTables():
-    pass
+    sql_query('''
+        DELETE FROM Teams;
+        DELETE FROM Players;
+        DELETE FROM Stadiums;
+    ''')
 
 
 def dropTables():
-    pass
+    # TODO: check if DROP use is legal
+    # TODO: why eilon used EXISTS and CASCADE
+    sql_query('''
+        DROP TABLE Teams;
+        DROP TABLE Players;
+        DROP TABLE Stadiums;
+    ''')
 
 
 def addTeam(teamID: int) -> ReturnValue:
-    pass
+    ret_val, _, _ = sql_query(f'''
+        INSERT INTO Teams VALUES({teamID});
+    ''')
+    return ret_val
 
 
 def addMatch(match: Match) -> ReturnValue:
-    pass
+    match_id = checkNone(match.getMatchID())
+    competition = checkNone(match.getCompetition())
+    home_id = checkNone(match.getHomeTeamID())
+    away_id = checkNone(match.getAwayTeamID())
+
+    ret_val, _, _ = sql_query(f'''
+        INSERT INTO Matches
+        VALUES({match_id}, {competition}, {home_id}, {away_id});
+    ''')
+    return ret_val
 
 
 def getMatchProfile(matchID: int) -> Match:
-    pass
+    ret_val, _, entries = sql_query(f'''
+            SELECT *
+            FROM Matches
+            WHERE MatchID = {matchID}
+        ''')
+    if ret_val != ReturnValue.OK:
+        return Match.badMatch()
+    else:
+        return Match(
+            matchID=entries.rows[0][0],
+            competition=entries.rows[0][1],
+            homeTeamID=entries.rows[0][2],
+            awayTeamID=entries.rows[0][3]
+        )
 
 
 def deleteMatch(match: Match) -> ReturnValue:
-    pass
+    ret_val, row_effected, _ = sql_query(f'''
+        DELETE FROM Matches
+        WHERE MatchID = {checkNone(match.getMatchID())}
+    ''')
+    if ret_val.rows_affected == 0:
+        return ReturnValue.NOT_EXISTS
+    return ret_val
 
 
 def addPlayer(player: Player) -> ReturnValue:
-    pass
+    ret_val, _, _ = sql_query(f'''
+            INSERT INTO Matches
+            VALUES({checkNone(player.getPlayerID())};
+        ''')
+    return ret_val
 
 
 def getPlayerProfile(playerID: int) -> Player:
-    pass
+    ret_val, _, entries = sql_query(f'''
+                SELECT *
+                FROM Players
+                WHERE PlayerID = {playerID}
+            ''')
+    if ret_val != ReturnValue.OK:
+        return Player.badPlayer()
+    else:
+        return Player(
+            playerID=entries.rows[0][0]
+        )
 
 
 def deletePlayer(player: Player) -> ReturnValue:
-    pass
+    ret_val, row_effected, _ = sql_query(f'''
+        DELETE FROM Players
+        WHERE MatchID = {checkNone(player.getPlayerID())}
+    ''')
+    if ret_val.rows_affected == 0:
+        return ReturnValue.NOT_EXISTS
+    return ret_val
 
 
 def addStadium(stadium: Stadium) -> ReturnValue:
-    pass
+    ret_val, _, _ = sql_query(f'''
+            INSERT INTO Stadiums
+            INSERT
+            VALUES({checkNone(stadium.getPlayerID())};
+        ''')
+    return ret_val
 
 
 def getStadiumProfile(stadiumID: int) -> Stadium:
-    pass
+    ret_val, _, entries = sql_query(f'''
+                    SELECT *
+                    FROM Stadiums
+                    WHERE StadiumID = {stadiumID}
+                ''')
+    if ret_val != ReturnValue.OK:
+        return Stadium.badStadium()
+    else:
+        return Stadium(
+            stadiumID=entries.rows[0][0],
+            competition=entries.rows[0][1],
+            homeTeamID=entries.rows[0][2],
+            awayTeamID=entries.rows[0][3]
+        )
 
 
 def deleteStadium(stadium: Stadium) -> ReturnValue:
-    pass
+    ret_val, row_effected, _ = sql_query(f'''
+            DELETE FROM Stadiums
+            WHERE StadiumID = {checkNone(stadium.getStadiumID())}
+        ''')
+    if ret_val.rows_affected == 0:
+        return ReturnValue.NOT_EXISTS
+    return ret_val
 
 
 def playerScoredInMatch(match: Match, player: Player, amount: int) -> ReturnValue:
---
+++ hw2_winter22/Tests/OurTest.py
@@ -0,0 +1,13 @@
+import unittest
+import Solution
+from Utility.ReturnValue import ReturnValue
+from Tests.abstractTest import AbstractTest
+from Business.Match import Match
+from Business.Stadium import Stadium
+from Business.Player import Player
+
+if __name__ == '__main__':
+    # Solution.createTables()
+    print("###############")
+    Solution.dropTables()
+    print("after")
